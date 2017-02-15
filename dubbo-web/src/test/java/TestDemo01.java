260160f6f2617c5425a05aa88d0ff2a5e190c2c5
yqlbd <yqlbd@sina.com>
2017-02-15 18:44:46 +0800

    20170208:增加了dubbo测试Test类，依赖API后即可注入。

---
+++ dubbo-web/src/test/java/TestDemo01.java
@@ -0,0 +1,33 @@
+import com.deppon.foss.module.pickup.pricing.api.server.service.IProductService;
+import com.deppon.foss.module.pickup.pricing.api.shared.domain.ProductEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by 343617 on 2017/2/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/dubbo-spring-test.xml")
+public class TestDemo01 {
+
+    @Resource
+    private IProductService productService;
+
+    @Test
+    public void test01(){
+//        ProductEntity productById = productService.findProductById("123");
+//        System.out.println(productById);
+//        Date lastModifyTime = productService.getLastModifyTime();
+//        System.out.println(lastModifyTime);
+        List<ProductEntity> productEntities = productService.queryLevel2ProductInfo();
+        for (ProductEntity productEntity : productEntities) {
+            System.out.println(productEntity.getName());
+        }
+    }
+}
